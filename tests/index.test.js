4eb397cab2b05cf80a536a4c34c37281b1cec16a
GLEBR1K <9glebr@gmail.com>
2019-09-29 12:23:36 +0300

    Use server helper in tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,27 +1,20 @@
-const net = require('net');
 const Mapper = require('../src/mapper');
+const server = require('../utils/server');
+
+const host = '127.0.0.1';
 const port = 5000;
 
 it(`map open ${port}`, function() {
-  const mapper = new Mapper('127.0.0.1', [port]);
-  return expect(runServer(port).then(function(server) {
+  return expect(server.run(host, port).then(function(serverInfo) {
+    const mapper = new Mapper(serverInfo.ip, [serverInfo.port]);
     return mapper.invoke().then(function(results) {
-      server.close();
+      serverInfo.server.close();
       return results;
     });
   })).resolves.toEqual([{ port: port, status: true}]);
 });
 
 it(`map closed ${port}`, function() {
-  const mapper = new Mapper('127.0.0.1', [port]);
+  const mapper = new Mapper(host, [port]);
   return expect(mapper.invoke()).resolves.toEqual([{ port: port, status: false}]);
 });
-
-function runServer(port) {
-  return new Promise(function(resolve) {
-    const server = net.createServer();
-    server.listen(port, '127.0.0.1', function() {
-      resolve(server);
-    });
-  });
-}
