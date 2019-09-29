83ace2ee33fb2aa858f72e84bb26fdec32cc8f31
GLEBR1K <9glebr@gmail.com>
2019-09-29 13:03:40 +0300

    Show version with -V param

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const cmd = require('commander');
 const range = require('parse-numeric-range');
 const Mapper = require('./src/mapper');
+const package = require('./package.json');
 
 cmd
   .arguments('<host> <ports>')
   .option('-o, --open', 'open only')
   .action(invokeMapper);
+
+cmd.version(package.version);
 
 cmd.parse(process.argv);
 
